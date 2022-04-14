82e11a2a7e6504bbaf5443a442a33ec0b61b35cd
Zach <zachdangelo100@gmail.com>
2022-04-14 12:01:44 -0400

    Created javascript to change through the pages.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ import createMenuPage from "./menu";
 import './style.css';
 import createContactPage from "./contact";
 
+var currentPage = "Home";
 
-
-createPage();
-createHomePage();
-clearPage();
-createMenuPage();
-clearPage();
-createContactPage();
 
 
 function clearPage(){
@@ -20,3 +14,42 @@ function clearPage(){
         bodyContainer.removeChild(bodyContainer.firstChild);
     }
 }
+
+function registerClickEvents(){
+    const homeLink = document.querySelector("#home-link");
+    homeLink.addEventListener("click", showHomePage);
+
+    const menuLink = document.querySelector("#menu-link");
+    menuLink.addEventListener("click",showMenuPage);
+
+    const contactLink = document.querySelector("#contact-link");
+    contactLink.addEventListener("click", showContactPage);
+}
+
+function showHomePage(){
+    if(currentPage != "Home"){
+        clearPage();
+        createHomePage();
+        currentPage = "Home";
+    }
+}
+
+function showMenuPage(){
+    if(currentPage != "Menu"){
+        clearPage();
+        createMenuPage();
+        currentPage = "Menu";
+    }
+}
+
+function showContactPage(){
+    if(currentPage != "Contact"){
+        clearPage();
+        createContactPage();
+        currentPage = "Contact";
+    }
+}
+
+createPage();
+createHomePage();
+registerClickEvents();
