8166c54c595e2b5a373098c29c647e5f77616a0b
Zach <zachdangelo100@gmail.com>
2022-04-14 10:18:06 -0400

    Home page created.

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import Image from './img.png';
+
 export default function createHomeSection(){
     const container = document.querySelector("#body-container");
     const infoContainer = document.createElement("div");
@@ -5,9 +7,19 @@ export default function createHomeSection(){
     
     const tagline = document.createElement("h2");
     tagline.textContent = "Best pizza outside of Pisa"
-    
-    const subTagline = document.createElement("h3");
+    infoContainer.appendChild(tagline);
+
+    const subTagline = document.createElement("h2");
     subTagline.textContent = "Est. circa 1170";
+    infoContainer.appendChild(subTagline);
 
+    const img = document.createElement("img");
+    img.src = Image;
+    infoContainer.appendChild(img);
+    
+    const callToAction = document.createElement("h2");
+    callToAction.textContent = "Order online or visit today!";
+    infoContainer.appendChild(callToAction);
 
+    container.appendChild(infoContainer);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import createPage from "./page";
+import createHomeSection from "./home";
 import './style.css';
 
 
 
 createPage();
+createHomeSection();
+
+
+function clearPage(){
+    const bodyContainer = document.querySelector("#body-container");
+    while(bodyContainer.firstChild){
+        bodyContainer.removeChild(bodyContainer.firstChild);
+    }
+}
