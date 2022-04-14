6e3ad1c1425bec6b97fb828166664e5ccb47b5e5
Zach <zachdangelo100@gmail.com>
2022-04-14 09:23:06 -0400

    Created webpack and node packages.

---
+++ src/page.js
@@ -0,0 +1,14 @@
+
+
+
+
+function createPage(){
+    const container = document.querySelector('#content');
+
+    const headerContainer = document.createElement('div');
+    headerContainer.classList.add('header-container');
+
+    
+
+
+};
